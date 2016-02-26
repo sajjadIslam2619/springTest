c0b2848ce2579078ecd6d56e72d3526d569423ac
sajjadIslam2619 <s.i.sajjad.islam@gmail.com>
2016-02-26 22:40:50 +0600

    first commit

---
+++ src/main/java/com/spring/test/testspring/App.java
@@ -0,0 +1,22 @@
+package com.spring.test.testspring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	ApplicationContext context=new FileSystemXmlApplicationContext("beans.xml");
+    	
+    	Patient patient=(Patient)context.getBean("patient_id");
+    	
+    	patient.speak();
+    	
+    	((FileSystemXmlApplicationContext)context).close();
+    }
+}
---
+++ src/main/java/com/spring/test/testspring/Patient.java
@@ -0,0 +1,9 @@
+package com.spring.test.testspring;
+
+
+public class Patient {
+	public void speak(){
+		System.out.println("help me!!");
+	}
+
+}
